dc5ffa202ecdb2c270a612561a6cad44371815cb
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2021-09-22 16:59:57 +0200

    panic: write to registers

--- rust-roottask/ws/roottask-bin/src/panic.rs
+++ rust-roottask/ws/roottask-bin/src/panic.rs
@@ -6,6 +6,7 @@ use core::sync::atomic::{
     Ordering,
 };
 
+/// Formats a nice panic message.
 fn generate_panic_msg(info: &PanicInfo) -> ArrayString<1024> {
     let mut buf = arrayvec::ArrayString::<1024>::new();
     // if this is an error, we ignore it. It would most probably mean, that the message
@@ -24,9 +25,26 @@ fn generate_panic_msg(info: &PanicInfo) -> ArrayString<1024> {
     buf
 }
 
+/// Writes 0x2EEDCOFFEE into r8 to r15, writes a nice panic message to the logger,
+/// and aborts the program in an endless loop.
 #[panic_handler]
 fn panic_handler(info: &PanicInfo) -> ! {
+    unsafe {
+        asm!(
+            "",
+            in("r8") 0x2EEDC0FFEE_u64,
+            in("r9") 0x2EEDC0FFEE_u64,
+            in("r10") 0x2EEDC0FFEE_u64,
+            in("r11") 0x2EEDC0FFEE_u64,
+            in("r12") 0x2EEDC0FFEE_u64,
+            in("r13") 0x2EEDC0FFEE_u64,
+            in("r14") 0x2EEDC0FFEE_u64,
+            in("r15") 0x2EEDC0FFEE_u64,
+        )
+    }
+
     log::error!("{}", generate_panic_msg(info));
+
     loop {
         compiler_fence(Ordering::SeqCst);
     }
