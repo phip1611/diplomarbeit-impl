729a7cb87ca7da67c88f1ef54e5aaa2d1e6e4c29
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2021-10-21 22:03:37 +0200

    libhedron: create_sc and QPD

---
+++ rust-roottask/ws/libhedron/src/qpd.rs
@@ -0,0 +1,43 @@
+//! Module for [`Qpd`].
+
+use crate::consts::NUM_PRIORITIES;
+
+/// Quantum Priority Descriptor (QPD).
+#[derive(Copy, Clone, Debug)]
+pub struct Qpd(u64);
+
+impl Qpd {
+    const QUANTUM_BITSHIFT: usize = 12;
+    const PRIORITY_BITMASK: usize = 0xff;
+
+    /// Creates a new object.
+    ///
+    /// # Parameters
+    /// * `priority` Priority between 1 and 128
+    /// * `quantum` time quantum in microseconds
+    pub fn new(priority: u64, quantum: u64) -> Self {
+        assert!(priority > 0, "priority must be bigger than 0");
+        assert!(
+            priority <= NUM_PRIORITIES as u64,
+            "priority must be lessequal to {}",
+            NUM_PRIORITIES
+        );
+        assert!(quantum > 0, "quantum must be bigger than 0");
+        let mut val = 0;
+        val |= priority & Self::PRIORITY_BITMASK as u64;
+        val |= quantum << Self::QUANTUM_BITSHIFT as u64;
+        Self(val)
+    }
+
+    pub const fn quantum(self) -> u64 {
+        self.0 >> Self::QUANTUM_BITSHIFT as u64
+    }
+
+    pub const fn priority(self) -> u64 {
+        self.0 & Self::PRIORITY_BITMASK as u64
+    }
+
+    pub const fn val(self) -> u64 {
+        self.0
+    }
+}
--- rust-roottask/ws/libhedron/src/syscall/create_sc.rs
+++ rust-roottask/ws/libhedron/src/syscall/create_sc.rs
@@ -1,9 +1,7 @@
 //! create_sc syscall
 
-use crate::capability::{
-    CapSel,
-    CrdNull,
-};
+use crate::capability::CapSel;
+use crate::qpd::Qpd;
 use crate::syscall::generic::{
     generic_syscall,
     SyscallNum,
@@ -14,15 +12,16 @@ pub fn create_sc(
     cap_sel: CapSel,
     owned_pd_sel: CapSel,
     bound_ec_sel: CapSel,
-    // scheduling_params: u64,
+    scheduling_params: Qpd,
 ) -> Result<(), SyscallStatus> {
     let mut arg1 = 0;
     arg1 |= SyscallNum::CreateSc.val() & 0xf;
     arg1 |= cap_sel << 8;
     let arg2 = owned_pd_sel;
     let arg3 = bound_ec_sel;
+    let arg4 = scheduling_params.val();
     unsafe {
-        generic_syscall(arg1, arg2, arg3, 0, 0)
+        generic_syscall(arg1, arg2, arg3, arg4, 0)
             .map(|_x| ())
             .map_err(|e| e.0)
     }
