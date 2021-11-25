5534aaf93b091579d607fa2937407b2a118198cf
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2021-11-25 13:49:33 +0100

    rust userland build: put static linux bins in userland tar ball

--- rust-roottask/build.sh
+++ rust-roottask/build.sh
@@ -13,6 +13,7 @@ cd "$DIR" || exit
 function fn_main() {
     fn_build_ws
     fn_cp_binaries_to_build_dir
+    fn_cp_linux_userland_payload
     fn_runtime_environment_tarball
 }
 
@@ -65,15 +66,27 @@ function fn_cp_binaries_to_build_dir() {
     done
 }
 
+function fn_cp_linux_userland_payload() {
+    (
+        cd "../static-hello-world" || exit
+        make
+        # merge build-directories
+        cp -r "build/" "../rust-roottask/"
+    )
+}
+
 # Builds the whole runtime environment into a tarball.
 # This means all executables in ./build except the roottask.
 function fn_runtime_environment_tarball() {
+    # Linux Files We
+    LINUX_BINS=$(cd "./build" || exit && find . -name "linux_*" | tr '\r\n' ' ')
+
     # debug
     (
         cd "./build" || exit
         # space separated string
         HRSTD_RT_DEBUG_FILES=$(find . -name "*.elf" ! -path . | grep "_debug_stripped" | grep -v "roottask" | tr '\r\n' ' ')
-        tar cfv "hedron-userland_debug.tar" $HRSTD_RT_DEBUG_FILES
+        tar cfv "hedron-userland_debug.tar" $HRSTD_RT_DEBUG_FILES $LINUX_BINS
     )
 
     # release
@@ -81,7 +94,7 @@ function fn_runtime_environment_tarball() {
         cd "./build" || exit
         # space separated string
         HRSTD_RT_RELEASE_FILES=$(find . -name "*.elf" ! -path . | grep "_release_stripped" | grep -v "roottask" | tr '\r\n' ' ')
-        tar cfv "hedron-userland_release.tar" $HRSTD_RT_RELEASE_FILES
+        tar cfv "hedron-userland_release.tar" $HRSTD_RT_RELEASE_FILES $LINUX_BINS
     )
 }
 
