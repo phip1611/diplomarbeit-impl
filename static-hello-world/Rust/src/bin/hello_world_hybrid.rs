79b8427da3a3568081613b49d6d6a70b1036e80e
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-01-29 17:37:23 +0100

    static bins: linux rust hello world hybrid adjusted to use latest libhrstd features

--- static-hello-world/Rust/src/bin/hello_world_hybrid.rs
+++ static-hello-world/Rust/src/bin/hello_world_hybrid.rs
@@ -1,52 +1,31 @@
-use libhrstd::cap_space::user::UserAppCapSpace;
 use libhrstd::kobjects::PdObject;
-use libhrstd::libhedron::mem::{PAGE_SIZE, USER_MAX_ADDR};
-use libhrstd::libhedron::syscall::create_ec::create_global_ec;
-use libhrstd::libhedron::syscall::create_pd::create_pd;
-use libhrstd::libhedron::utcb::Utcb;
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
 use std::env::var;
-use std::rc::Rc;
-
-const USER_UTCB_ADDR: u64 = (USER_MAX_ADDR - PAGE_SIZE) as u64;
-const HEDRON_NATIVE_SYSCALL_MAGIC: u64 = 1 << 63;
-
-/// Wraps a single Hedron syscall. The code inside `actions`
-/// should not do any more than the raw syscall. Things as
-/// unwrapping on an `Err` will already break everything,
-/// because the panic handler might write to stderr whereas
-/// Hedron still things, the application tries to make Hedron-
-/// native syscalls.
-fn wrap_hedron_syscall<T, R>(actions: T) -> R
-where
-    T: Fn() -> R,
-{
-    utcb_mut().set_head_tls(HEDRON_NATIVE_SYSCALL_MAGIC);
-    let res = actions();
-    utcb_mut().set_head_tls(0);
-    res
-}
 
 fn main() {
-    println!("Hello, world!");
+    // to get log messages from libhrstd
+    SimpleLogger::new()
+        // prevent not supported "clock_gettime"
+        // syscall on Hedron :)
+        .without_timestamps()
+        .with_level(LevelFilter::max())
+        .init()
+        .unwrap();
+
+    println!("Hello world from hybrid Linux application written in Rust!");
 
     if var("LINUX_UNDER_HEDRON").is_ok() {
         println!("This Linux binary executes under Hedron");
-        let new_pd = create_new_pd();
-        println!("created new PD: {:#?}", new_pd);
+        let self_pd = PdObject::self_in_user_cap_space(1);
+        println!("Executing Hedron-native system call now:");
+        let new_pd = PdObject::create(2, &self_pd, 1000, None);
+        println!(
+            "Executed Hedron-native system call successfully! new PD: {:#?}",
+            new_pd
+        );
+        println!("hybrid part done");
     } else {
         println!("This Linux binary executes under native Linux");
     }
-}
-
-/// Loads the UTCB from the well-known location in user apps.
-/// Currently: Shortcut, only one UTCB for main thread.
-fn utcb_mut() -> &'static mut Utcb {
-    unsafe { (USER_UTCB_ADDR as *mut Utcb).as_mut().unwrap() }
-}
-
-fn create_new_pd() -> Rc<PdObject> {
-    let self_pd = PdObject::self_in_user_cap_space(1);
-    let pd = PdObject::new(self_pd.pid() + 1, Some(&self_pd), 1000);
-    wrap_hedron_syscall(|| create_pd(false, pd.cap_sel(), self_pd.cap_sel(), None)).unwrap();
-    pd
 }
