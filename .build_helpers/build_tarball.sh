3a1459883783a9860e6e2a8542e8abcad6bcc87f
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-02-06 13:00:00 +0100

    prevent userland tarball from containing itself :D

--- .build_helpers/build_tarball.sh
+++ .build_helpers/build_tarball.sh
@@ -18,6 +18,10 @@ USERLAND_FILES=$(find . \
   `# make sure we don't search for files in './build/musl'` \
   -maxdepth 1 \
   -type f \
+  `# exclude files that start with dot (hidden files)` \
+  ! -path '*/.*' \
+  `# exclude the userland.tar (otherwise the tar gets exponentially bigger :D)` \
+  ! -path '*/userland.tar' \
   `# exclude Roottask` \
   | grep -v "roottask" \
   `# exclude Hedron` \
