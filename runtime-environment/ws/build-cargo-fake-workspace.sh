706680328dab016e49f64e34552a7c00d3603a5b
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2022-02-07 11:56:20 +0100

    runtime environment build: drop slightly faster build for build stability and developer productivity

--- runtime-environment/ws/build-cargo-fake-workspace.sh
+++ runtime-environment/ws/build-cargo-fake-workspace.sh
@@ -14,41 +14,22 @@ cd "$DIR" || exit
 #########################################################################
 
 function fn_main() {
-    # find all direct directories without "." and without libhedron (see below)
-    LIBS=$(find . -maxdepth 1 -type d ! -path . ! -name "libhedron" -name "lib*")
+    # find all direct directories without "."
+    LIBS=$(find . -maxdepth 1 -type d ! -path . -name "lib*")
     BINS=$(find . -maxdepth 1 -type d ! -path . -name "*-bin")
 
-    # I trigger this build here extra so that Cargo has time to install its custom
-    # toolchain if it is not present. Otherwise, if someone doesn't have the toolchain
-    # installed yet, the build will fail when all Rust builds start in parallel and
-    # try to update the systems toolchain.
-    fn_build_rust_lib libhedron
+    # I tried to parallelize this with "&" (put into background), but this
+    # is error prone and breaks much developer convenience. The output
+    # gets so polluted that it doesn't really stop there where it found
+    # a compilation error.
 
-    set +e
-    PIDS=()
-    PROJECTS=()
     for LIB in $LIBS; do
-        fn_build_rust_lib "$LIB" &
-        # $! contains the PID of the just started process
-        # if the process was put into background with "&"
-        PID=$!
-        PIDS+=("$PID")
-        PROJECTS+=("$LIB")
+        fn_build_rust_lib "$LIB"
     done
 
     for BIN in $BINS; do
-        fn_build_rust_bin "$BIN" &
-        # $! contains the PID of the just started process
-        # if the process was put into background with "&"
-        PID=$!
-        PIDS+=("$PID")
-        PROJECTS+=("$BIN")
+        fn_build_rust_bin "$BIN"
     done
-
-    # to optimize build time, I build everything
-    # in parallel; "&" creates a bacground task
-    # for every compilation!
-    wait_and_check $PIDS $PROJECTS
 
     fn_build_extra_checks
 }
@@ -85,24 +66,6 @@ function fn_build_extra_checks() {
         cd "libhrstd" || exit
         cargo check --no-default-features --features foreign_rust_rt
     )
-}
-
-# Waits fo all
-function wait_and_check() {
-    PIDS=$1
-    PROJECTS=$2
-
-    # same len for arrays PIDS and PROJECTS
-    LEN="${#PIDS[@]}"
-
-    for ((i = 0; i < LEN; i++)); do
-        if wait "${PIDS[i]}"; then
-            echo "${PROJECTS[i]} success"
-        else
-            echo "${PROJECTS[i]} failure!"
-            exit 1
-        fi
-    done
 }
 
 fn_main
